b0dfb898620e034e5654e6f8a5877ca5bde1ce9c
DavidAtBede <david.thompson@bedegaming.com>
2024-01-23 13:37:23 +0000

    Added a test project (NUnit)

---
+++ AppForActions/ASketch.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppForActions
+{
+    public class ASketch
+    {
+        public string SayMyName() => "David";
+    }
+}
---
+++ Tests/TheTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace Tests;
+
+[TestFixture]
+public class TheTests
+{
+    [Test]
+    public void TheOneAndOnlyTest()
+    {
+        Assert.That(1 == 1, "Oh!");
+    }
+}
+
